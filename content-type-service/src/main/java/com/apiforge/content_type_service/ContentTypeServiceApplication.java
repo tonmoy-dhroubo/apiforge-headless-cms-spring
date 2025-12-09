3e29d09c0f63f494fcb4135b5aa91d62823c9da7
Tonmoy Ahmed Dhroubo <tonmoyahmeddhroubo@gmail.com>
2025-12-10 01:06:24 +0600

    Initial commit

---
+++ content-type-service/src/main/java/com/apiforge/content_type_service/ContentTypeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apiforge.content_type_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentTypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentTypeServiceApplication.class, args);
+	}
+
+}
